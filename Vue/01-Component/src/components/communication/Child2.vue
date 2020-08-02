3ad9c89e8e88dd2ff418ad8f714eeecddacd9aa5
Domesy <397163123@qq.com>
2020-08-03 05:36:55 +0800

    组件通信，和

---
+++ Vue/01-Component/src/components/communication/Child2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>child2</h3>
+    <!-- $attrs: 只要是没有被props声明都会在$attrs中 -->
+    <p>{{$attrs.msg}}</p>
+    <!-- 隔代传参 v-bind会展开$attrs-->
+    <Grandson v-bind="$attrs" v-on="$listeners"/>
+
+
+    <!-- <button @click="sentToChild1">给child1发消息</button> -->
+  </div>
+</template>
+
+<script>
+  import  Grandson from '@/components/communication/Grandson'
+  export default {
+    created(){
+      // console.log(this.$attrs)
+    },
+    components:{
+      Grandson
+    },
+    methods:{
+      sentToChild1(){
+        // 利用事件总线发送消息
+        this.$bus.$emit('event-from-child2', 'some msg from child2')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Vue/01-Component/src/components/communication/Grandson.vue
@@ -0,0 +1,33 @@
+<template>
+  <div @click="$emit('some-event','msg from grandson')">
+    <h3>grandson</h3>
+    <p>msg</p>
+
+    <!-- inject -->
+    <p>{{foo1}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    // inject: ['foo'],
+    inject: {
+      foo1: {
+        from: 'foo'
+      }
+    },
+    created(){
+      // console.log(this.foo)
+    },
+    props: {
+      msg: {
+        type: String,
+        default: ''
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
