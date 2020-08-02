851e609b7359163effd5a694bd39b0a947c0b8dc
Domesy <397163123@qq.com>
2020-08-03 05:38:32 +0800

    01-Component

---
+++ Vue/01-Component/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="scss" >
+#app{
+  text-align: center;
+  margin: 0 auto;
+  // background: $color-gray;
+  height: 100vh;
+  // width: 100%;
+}
+</style>
---
+++ Vue/01-Component/src/view/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- 组件通信 -->
+    <!-- <Communication ></Communication> -->
+
+    <!-- 插槽 -->
+    <!-- <Solt></Solt> -->
+
+    <!-- Element表单 -->
+    <ElementTest ></ElementTest>
+
+
+  </div>
+</template>
+
+<script>
+  import Communication from '@/components/communication';
+  import Solt from  '@/components/solt';
+  import ElementTest from '@/components/form'
+
+  export default {
+    name: 'Index',
+    props:{
+      name: String
+    },
+    components:{
+      Communication,
+      Solt,
+      ElementTest
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
