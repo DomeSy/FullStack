86fe8ecafd565b5dbd376adc85d281d326bc441b
Domesy <397163123@qq.com>
2020-09-17 15:59:28 +0800

    Provide导入所有的数据

--- React/03-Familybucket/src/App.js
+++ React/03-Familybucket/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReduxPage from './view/Rudex/ReduxPage';
+import ReactReduxPage from './view/ReactRedux/ReactReduxPage';
+// import ReduxPage from './view/Rudex/ReduxPage';
 // import ReducerPage from './view/Rudex/ReducerPage';
+// import HookPages from './view/Hook/HookPages';
 
 function App() {
   return (
     <div>
       {/* Redux */}
-      <ReduxPage />
+      {/* <ReduxPage /> */}
 
       {/* Reducer */}
       {/* <ReducerPage /> */}
+
+      {/* Hook */}
+      {/* <HookPages /> */}
+
+      {/* React-Redux */}
+      <ReactReduxPage />
     </div>
   );
 }
