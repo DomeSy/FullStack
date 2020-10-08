2bb739796d68ad2ff09fcf4cb033fbc30ed0c3b1
Domesy <397163123@qq.com>
2020-10-08 17:17:37 +0800

    Hook API useContext useState useEffect

--- React/03-Familybucket/src/view/Hook/FruiteAdd.js
+++ React/03-Familybucket/src/view/Hook/FruiteAdd.js
@@ -1,15 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useContext } from 'react'
 
 // 用户输入水果，回车后水果列表增加
-export default function FruiteAdd({onAdd}) {
+export default function FruiteAdd({
+  // onAdd, 
+  Context
+}) {
   const [fname, setFname] = useState("")
+  // Context
+  const { fruits, setFruits } = useContext(Context)
   
   const onAddFruite = (e) => {
     if(e.key === "Enter"){
-      onAdd(fname);
+      // onAdd(fname);
+
+      // context
+      setFruits([...fruits, fname])
+
       setFname("")
     }
   }
+
 
   return (
     <div>
