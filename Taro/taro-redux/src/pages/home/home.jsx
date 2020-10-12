cc8931ab44bf3e963af289a79d342e310c554bdb
Domesy <397163123@qq.com>
2020-10-13 00:07:28 +0800

    taro多端配置

---
+++ Taro/taro-redux/src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View } from '@tarojs/components';
+import './home.scss'
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+        首页
+      </View>
+    )
+  }
+}
+
+export default Home
---
+++ Taro/taro-redux/src/pages/service/service.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View } from '@tarojs/components';
+import './service.scss'
+
+class Service extends Component {
+  render() {
+    return (
+      <View>
+        服务
+      </View>
+    )
+  }
+}
+
+export default Service
